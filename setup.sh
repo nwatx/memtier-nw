3a232baa135125846033b329036d7d58aea4674d
nw (remote) <neow@utexas.edu>
2025-01-11 15:06:46 -0700

    fixes + setup scripts

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo apt-get install linux-tools-5.15.0-122-generic linux-cloud-tools-5.15.0-122-generic
+sudo mount -o remount,mode=755 /sys/kernel/tracing/
+echo "kernel.perf_event_paranoid = -1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+mkdir build
+cd build
+cmake ..
+mkdir data
+touch ./data/trace.data
+sudo chmod -R 777 ./data/trace.data
--- src/basic_array.cpp
+++ src/basic_array.cpp
@@ -1,7 +1,8 @@
 #include <vector>
+#include <cstdlib>
 
 int main() {
-    int v_size = static_cast<int>(1e8);
+    int v_size = static_cast<int>(1e9);
     std::vector<int> v(v_size);
     for (int i = 0; i < v_size; ++i) {
         v[i] = std::rand();
